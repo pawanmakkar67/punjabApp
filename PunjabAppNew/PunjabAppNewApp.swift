6d6e3778b9bf77f45bb5ce589bb73a29d00813f0
pawanpreet <pawanmakkar67@gmail.com>
2025-10-29 19:37:34 +0530

    Initial Commit

---
+++ PunjabAppNew/PunjabAppNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunjabAppNewApp.swift
+//  PunjabAppNew
+//
+//  Created by pc on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PunjabAppNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
